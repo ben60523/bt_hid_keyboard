dc8a345b821afa810f0cc465eb58c1e48b0d9922
ben60523 <ben60523@gmail.com>
2024-11-14 13:20:02 +0800

    Use enumerate in MacOS

--- main.cc
+++ main.cc
@@ -5,32 +5,33 @@
 Napi::Value enumerate(const Napi::CallbackInfo &info)
 {
   Napi::Env env = info.Env();
-
-  struct hid_device_info *devs, *cur_dev;
-  char foundpath[1024]; // 1024 should be enough for anyone :)
-
-  devs = hid_enumerate(0x0, 0x0);
-  cur_dev = devs;
+  struct hid_device_info *devices, *cur_dev;
+  devices = hid_enumerate(0x0, 0x0);
+  cur_dev = devices;
   while (cur_dev) {
-    printf("Device: vid/pid: %04hx/%04hx\n  path: %s\n  serial_number: %ls usage_page: %x, usage: %x",
-      cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number, cur_dev->usage_page, cur_dev->usage);
-    printf("\n");
-    printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
-    printf("  Product:      %ls\n", cur_dev->product_string);
-    printf("\n");
-    if( cur_dev->vendor_id == 0x4e8 && cur_dev->product_id == 28705 ) { // && cur_dev->usage_page == 0xFFAB && cur_dev->usage == 0x200 ) {
-      printf("Found our device!\n");
-      strcpy( foundpath, cur_dev->path );
+    // printf("Device: vid/pid: %04hx/%04hx\n  path: %s\n  serial_number: %ls usage_page: %x, usage: %x",
+    //   cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number, cur_dev->usage_page, cur_dev->usage);
+    // printf("\n");
+    // printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
+    // printf("  Product:      %ls\n", cur_dev->product_string);
+    // printf("\n");
+    if( cur_dev->vendor_id == 0x4e8 && cur_dev->product_id == 28705 ) {
+      hid_free_enumeration(devices);
+      printf("%ls\n", cur_dev->product_string);
+      return Napi::Boolean::New(env, true);
     }
     cur_dev = cur_dev->next;
   }
-  hid_free_enumeration(devs);
-  return Napi::Boolean::New(env, true);
+  hid_free_enumeration(devices);
+  return Napi::Boolean::New(env, false);
 }
 
 Napi::Value checkBTPedal(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
+# ifdef __APPLE__
+    return enumerate(info);
+# else
     hid_init();
     hid_device *handle = hid_open(0x4e8, 28705, NULL);
     if (!handle)
@@ -46,6 +47,7 @@ Napi::Value checkBTPedal(const Napi::CallbackInfo &info)
         hid_exit();
         return Napi::Boolean::New(env, true);
     }
+#endif
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports)
